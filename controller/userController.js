77f188f725df78f2f8fbaf45b04a2d812dc91eb4
vishal-07 <dubeyvishal714@gmail.com>
2025-06-11 00:22:39 +0530

    signup created

--- controller/userController.js
+++ controller/userController.js
@@ -1,13 +1,30 @@
-import bycrpt from "bycrpt" ;
+import bcrypt from "bcyrpt" ;
 import User from "../model/user";
 
 
 const userSignUp = async(req , res)=>{
     try{ 
-        const hashcode = await bcrypt.hash(req.body.password , 10);
-        
+        const hashedPassword = await bcrypt.hash(req.body.password , 10);
+        const uplaodImage = await cloudinary.uploader.upload(
+            req.files.logo.tempFilePath
+        )
+        const newUser = new User({
+            _id : new mongoose.Types.ObjectId,
+            email:req.body.email ,
+            password : hashedPassword,
+            channelName : req.body.channelName,
+            phone : req.body.phone ,
+            logoUrl : uploadImage.secure_url,
+            logoId : uploadImage.public_Id
+            
+        });
+        const user = await newUser.save();
+
+        res.status(201).json({
+            user
+        })
     }
     catch(e){
-
+        console.error("Something went wrong due to ", e);
     }
 }
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const connectToDB = require("./database/db");
 require("dotenv").config()
+import fileUpload from "express-fileupload";
+import bodyParser from "body-parser";
 
 const PORT = process.env.PORT || 8000 ;
 connectToDB()
@@ -10,6 +12,12 @@ connectToDB()
 
 const app = express();
 app.use("/api/v1/user",userRoutes);
+app.use(bodyParser.json());
+
+app.use(fileUpload({
+    useTempFiles : true ,
+    tempFileDir : "./tmp/"
+}))
 
 app.listen(PORT , ()=>{
     console.log(`Server is running on ${PORT}`);
