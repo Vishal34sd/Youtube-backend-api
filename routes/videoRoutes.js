038aa0cf836a57b83c69a86b549ef1ab53eff6d0
vishal-07 <dubeyvishal714@gmail.com>
2025-06-12 22:59:38 +0530

    video upload feature added

--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -1,4 +1,4 @@
-import mongoose from "monggose";
+import mongoose from "mongoose";
 import express from "express";
 import User from "../model/user.js";
 import Video from "../model/video.js";
@@ -7,7 +7,45 @@ import checkAuth from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/upload",checkAuth, )
+router.post("/upload",checkAuth,async(req, res)=>{
+    try{
+        const {title , description, category ,tags }= req.body; 
+        if(!req.files || req.files.video || req.files.thumbnail){
+            res.status(400).json({
+                error : "Video and thumbnail are required"
+            });
+        }
+        const videoUpload = await cloudinary.uploader.upload(req.files.video.tempFilePath,{
+            resource_type : "video",
+            folder:"videos"
+        });
+
+         const thumbnailUpload = await cloudinary.uploader.upload(req.files.thumbnail.tempFilePath,{
+        
+            folder:"thumbnails"
+        });
+
+        const newVideo = new Video({
+            _id : req.user._id,
+            videoUrl : videoUpload.secure_url,
+            videoId : videoUpload.publicId ,
+            thumbnailUrl : thumbnailUpload.secure_url,
+            thumbnailId : thumbnailUpload.public_id,
+            category ,
+            tags : tags ? tag.split(","): []
+        });
+        await newVideo.save();
+        res.status(200).json({
+            message : "Video Uploaded successfully ", video : newVideo
+        });
+    }
+        catch(e){
+       console.error("Something went wrong due to ", e);
+        res.status(500).json({ error: "Internal server error" });
+    } 
+});
+
+router.put("/update/:id", checkAuth , )
 
 
 
