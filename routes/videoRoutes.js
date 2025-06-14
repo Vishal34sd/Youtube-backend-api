6df5a5c9ffc79c602d945b7f1b018f5d5866fb00
vishal-07 <dubeyvishal714@gmail.com>
2025-06-14 19:12:53 +0530

    Video update feature added

--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -44,8 +44,47 @@ router.post("/upload",checkAuth,async(req, res)=>{
         res.status(500).json({ error: "Internal server error" });
     } 
 });
+//No video wiull change only its meta data will change 
+router.put("/update/:id", checkAuth , async(req , res )=>{
+    try{
+        const {title , description , category , tags } = req.body;
+        const videoId = req.params.id ;
+        // find video by id 
+        const video = await Video.findById(videoId);
+        if(!video){
+            res.status(400).json({
+                message : "Video not found "
+            });
+        }
+        if(video.user_id.toString()!==req.user._id.toString()){
+            res.status(403).json({
+                message : "Unauthorized Access"
+            });
+        }
+        if(req.files && req.files.thumbnail){
+            await cloudinary.uploader.destroy(video.thumbnailId);
 
-router.put("/update/:id", checkAuth , )
+            const thumbnailUpload = await cloudinary.uploader.upload(req.files.thumbnail.tempFilePath,{
+                folder: "thumbnail"
+            });
+        video.thumbnailUrl = thumbnailUpload.secure_url;
+        video.thumbnailId = thumbnailUpload.public_id;
+        }
+        video.title = title || video.title;
+        video.description = description || video.description;
+        video.category = category || video.category;
+        video.tags = tag? tags.split(","): video.tags;
+
+        await video.save();
+        res.status(200).json({
+            message : "Video updated successfully", video
+        });
+    }
+    catch(e){
+         console.error("Something went wrong due to ", e);
+        res.status(500).json({ error: "Internal server error" });
+    }
+} )
 
 
 
